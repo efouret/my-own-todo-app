7fcbdac232932245097135bb68c9d27587fb81d6
Erwan Fouret <erwan.fouret@gmail.com>
2024-10-06 18:03:41 +0200

    fix(frontend): make local npm run dev work

--- frontend/my-own-todo-app/vite.config.ts
+++ frontend/my-own-todo-app/vite.config.ts
@@ -1,9 +1,17 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { sveltekit } from '@sveltejs/kit/vite'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
-});
+		include: ['src/**/*.{test,spec}.{js,ts}'],
+	},
+	server: process.env.NODE_ENV === 'development' ? {
+		proxy: {
+			'/api': {
+				target: 'http://localhost:8080',
+				changeOrigin: true,
+			},
+		},
+	} : undefined,
+})
